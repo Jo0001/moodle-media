ba24fc057ef54070ceda9c101cb33a29a83ab13b
Jo0001 <github-jo0001@gmx-topmail.de>
2020-05-11 14:05:46 +0200

    Add files via upload

---
+++ moodle-media.js
@@ -0,0 +1,17 @@
+let els = document.querySelectorAll('a');
+for (let i = els.length; i--;) {
+    if (els[i].href.includes(".mp3")) {
+        let sound = document.createElement('audio');
+        sound.controls = 'controls';
+        sound.src = els[i].href;
+        sound.type = 'audio/mpeg';
+        els[i].appendChild(sound);
+    } else if (els[i].href.includes(".mp4")) {
+        let video = document.createElement('video');
+        video.controls = 'controls';
+        video.src = els[i].href;
+        video.type = 'video/mp4';
+        video.style = 'height:150px';
+        els[i].appendChild(video);
+    }
+}
