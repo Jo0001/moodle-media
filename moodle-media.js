3b42f90285cc7fede24895ff20730d82a6e2f209
Jo0001 <47455182+Jo0001@users.noreply.github.com>
2020-07-23 22:36:40 +0200

    Improved element inserting

--- moodle-media.js
+++ moodle-media.js
@@ -5,13 +5,13 @@ for (let i = els.length; i--;) {
         sound.controls = 'controls';
         sound.src = els[i].href;
         sound.type = 'audio/mpeg';
-        els[i].appendChild(sound);
+        els[i].parentElement.appendChild(sound);
     } else if (els[i].href.includes(".mp4")) {
         let video = document.createElement('video');
         video.controls = 'controls';
         video.src = els[i].href;
         video.type = 'video/mp4';
         video.style = 'height:150px';
-        els[i].appendChild(video);
+        els[i].parentElement.appendChild(video);
     }
 }
